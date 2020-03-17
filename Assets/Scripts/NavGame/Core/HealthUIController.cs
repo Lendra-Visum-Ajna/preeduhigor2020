a4fac98ff0d6f2265cecc9bb5d013d3e78040ca3
Lendra Visum <higorfelipe127@gmail.com>
2020-03-17 11:53:26 -0300

    Health Events

--- Assets/Scripts/NavGame/Core/HealthUIController.cs
+++ Assets/Scripts/NavGame/Core/HealthUIController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 
@@ -16,6 +17,8 @@ namespace NavGame.Core
         GameObject healthUI;
 
         Transform cam;
+        DamageableGameObject damageable;
+        Image healthSlider;
         void Awake()
         {
             Canvas canvas = FindWorldCanvas();
@@ -25,6 +28,11 @@ namespace NavGame.Core
             }
             cam = Camera.main.transform;
             healthUI = Instantiate(healthUIPrefab, canvas.transform);
+            healthSlider = healthUI.transform.GetChild(0).GetComponent<Image>();
+            damageable = GetComponent<DamageableGameObject>();
+
+            damageable.onHealthChanged += UpdateHealth;
+            damageable.onDied += DestroyHealth;
 
         }
 
@@ -46,6 +54,19 @@ namespace NavGame.Core
                 }
             }
             return null;
+        }
+        void UpdateHealth(int maxHealth, int currentHealth)
+        {
+            if (healthUI != null)
+            {
+                float healthPercent = (float) currentHealth / maxHealth;
+                healthSlider.fillAmount = healthPercent;
+            }
+        }
+
+        void DestroyHealth()
+        {
+            Destroy(healthUI);
         }
 
     }
